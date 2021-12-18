f0abcfc0571dcf1567ea76908a0b17e4782902d1
Francesco Di Sales <f.disales@hotmail.com>
2021-12-18 19:45:58 +0100

    Update project.sql

--- sql/project.sql
+++ sql/project.sql
@@ -1,11 +1,7 @@
 CREATE TABLE project
 (
-
-	badgeid CHAR(4),
 	code_project CHAR(4),
 	name_project VARCHAR(50) NOT NULL,
 	
-	PRIMARY KEY(code_project),
-	foreign key (badgeid) references developer(badgeid)
-
+	PRIMARY KEY(code_project)
 );
