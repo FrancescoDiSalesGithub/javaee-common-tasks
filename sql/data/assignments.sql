bd8f49b1661e0c32b868ffd3658339507590f8b5
Francesco Di Sales <f.disales@hotmail.com>
2021-12-18 20:03:10 +0100

    Create assignments.sql

---
+++ sql/data/assignments.sql
@@ -0,0 +1,5 @@
+insert into assignmentproject values('1234','ms01');
+insert into assignmentproject values ('5678','ms01');
+
+insert into assignmentproject  values ('1213','hkos');
+insert into assignmentproject  values ('1416','hkos');
