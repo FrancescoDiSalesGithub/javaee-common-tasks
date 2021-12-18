c29fb75eb1d45fd29756af5e001328999c2f9a69
Francesco Di Sales <f.disales@hotmail.com>
2021-12-18 19:39:25 +0100

    Create developers.sql

---
+++ sql/data/developers.sql
@@ -0,0 +1,5 @@
+insert into developer  values ('1234','Bill','Gates',25);
+insert into developer values('5678','Steve','Ballmer',26);
+insert into developer values ('9101','John','Doe',22);
+insert into developer values ('1213','Elliot','Alderson',25);
+insert into developer values ('1416','Darlene','Alderson',25);
